f285eea9a3ad7785f7a3ee8b1add2d11a21bbc80
Unknown <19pipas@gmail.com>
2016-11-20 18:33:19 +0000

    Fixed bugs and reworked documentation

---
+++ documentacao/html/search/all_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['infoinicial',['infoInicial',['../_menus_8cpp.html#a978c7557eca55fb477d0e41bf13b2da9',1,'infoInicial(string &amp;fichClientes, string &amp;fichTransacoes, string &amp;fichOrdensVenda, string &amp;fichOrdensCompra):&#160;Menus.cpp'],['../_menus_8h.html#a978c7557eca55fb477d0e41bf13b2da9',1,'infoInicial(string &amp;fichClientes, string &amp;fichTransacoes, string &amp;fichOrdensVenda, string &amp;fichOrdensCompra):&#160;Menus.cpp']]],
+  ['insertionsort',['insertionSort',['../insertion_sort_8h.html#a9bec216ad15dc4199bf30ade4400bc1a',1,'insertionSort.h']]],
+  ['insertionsort_2eh',['insertionSort.h',['../insertion_sort_8h.html',1,'']]]
+];
--- src/Doxygen documentation/html/search/functions_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['infoinicial',['infoInicial',['../_menus_8cpp.html#a978c7557eca55fb477d0e41bf13b2da9',1,'infoInicial(string &amp;fichClientes, string &amp;fichTransacoes, string &amp;fichOrdensVenda, string &amp;fichOrdensCompra):&#160;Menus.cpp'],['../_menus_8h.html#a978c7557eca55fb477d0e41bf13b2da9',1,'infoInicial(string &amp;fichClientes, string &amp;fichTransacoes, string &amp;fichOrdensVenda, string &amp;fichOrdensCompra):&#160;Menus.cpp']]],
-  ['insertionsort',['insertionSort',['../insertion_sort_8h.html#a2d750432a373f9dab8039bef160b71a0',1,'insertionSort.h']]]
-];
